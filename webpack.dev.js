5c83db8b76e5be185e0cd1e980bfd61ed24aeac4
Malik Felix <malikfelix50@gmail.com>
2021-01-25 10:34:11 -0500

    add webpack-dev-server

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,8 @@
-const path = require('path')
-const webpack = require('webpack')
-const HTMLWebPackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   mode: 'development',
@@ -19,6 +21,16 @@ module.exports = {
     new HTMLWebPackPlugin({
       template: './src/client/views/index.html',
       filename: './index.html',
-    })
+    }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write logs to the console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false
+    }),
+    new BundleAnalyzerPlugin()
   ]
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
-const path = require('path')
-const webpack = require('webpack')
-const HTMLWebPackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -18,6 +19,15 @@ module.exports = {
     new HTMLWebPackPlugin({
       template: './src/client/views/index.html',
       filename: './index.html',
+    }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write logs to the console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false
     })
   ]
 }
